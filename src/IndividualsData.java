8ecc220dfa693ca2bc106435f0a9f308868c6f87
mrebori <167725474+mrebori@users.noreply.github.com>
2024-04-28 23:09:55 -0400

    Module 1: Critical Thinking
    Option #1: List Individual's Name and Address Information.

---
+++ src/IndividualsData.java
@@ -0,0 +1,17 @@
+public class IndividualsData {
+
+    public static void main (String[] args) {
+
+        String firstName = "Michael"; // Declare and assign individual's firstName
+        String lastName = "Scott"; // Declare and assign individual's lastName
+        String streetAddress = "1725 Slough Ave."; // Declare and assign individual's streetAddress
+        String city = "Scranton"; // Declare and assign individual's city
+        String zipCode = "18501"; // Declare and assign individual's zipCode
+
+        System.out.println(firstName); // Print individual's firstName and start a new line
+        System.out.println(lastName); // Print individual's lastName and start a new line
+        System.out.println(streetAddress); // Print individual's streetAddress and start a new line
+        System.out.println(city); // Print individual's city and start a new line
+        System.out.println(zipCode); // Print individual's zipCode and start a new line
+    }
+}
